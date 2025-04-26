6a6ca99b30cd77733d7a257a24d97cdd814f031e
AmiNilo <aminilo.dev@gmail.com>
2025-04-27 02:49:10 +0330

    Add banner to prettify CLI

--- src/cli.ts
+++ src/cli.ts
@@ -2,5 +2,13 @@
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 import installCommand from "./commands/install";
+import figlet from 'figlet';
+import chalk from 'chalk';
+
+(()=> {
+	/* Doom Epic Ogre Puffy Rounded Slant Speed */
+	console.log(chalk.magenta(figlet.textSync(' Package  Git  Initializer', { font: 'Ogre', horizontalLayout: 'full' })));
+	console.log(chalk.green(' 🚀 Pick your favorite packages and embark!\n'))
+})();
 
 yargs(hideBin(process.argv)).command(installCommand).demandCommand(1, "You need to provide a valid command").strict().help().argv;
