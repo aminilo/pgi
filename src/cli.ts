1c31f2943752520a1f7370219664a5564530a447
AmiNilo <aminilo.dev@gmail.com>
2025-04-27 06:03:08 +0330

    1.0.1

--- src/cli.ts
+++ src/cli.ts
@@ -7,8 +7,8 @@ import chalk from 'chalk';
 
 (()=> {
 	/* Doom Epic Ogre Puffy Rounded Slant Speed */
-	console.log(chalk.magenta(figlet.textSync(' Package  Git  Initializer', { font: 'Ogre', horizontalLayout: 'full' })));
-	console.log(chalk.green(' 🚀 Pick your favorite packages and embark!\n'))
+	console.log(chalk.magenta(figlet.textSync('PGI', { font: 'Speed', horizontalLayout: 'full' })));
+	console.log(chalk.green(` 🚀 ${chalk.bold('Package Git Initializer')} - Pick your favorite packages and embark!\n`));
 })();
 
 yargs(hideBin(process.argv)).command(installCommand).demandCommand(1, "You need to provide a valid command").strict().help().argv;
